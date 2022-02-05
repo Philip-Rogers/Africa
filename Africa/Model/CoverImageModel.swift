61d0d25447bbdc7297653c2ddef9c27d8a3aec85
spaschotic <87228774+spaschotic@users.noreply.github.com>
2022-02-06 01:44:18 +1000

    init

---
+++ Africa/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by Philip Rogers on 6/2/22.
+//
+
+import SwiftUI
+
+struct CoverImage: Codable, Identifiable {
+    let id: Int
+    let name: String
+}
