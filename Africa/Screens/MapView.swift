3767ddd3bf4c721c00a6d1ecb2c51c62b33500b9
spaschotic <87228774+spaschotic@users.noreply.github.com>
2022-02-06 00:15:42 +1000

    init

---
+++ Africa/Screens/MapView.swift
@@ -0,0 +1,20 @@
+//
+//  MapView.swift
+//  Africa
+//
+//  Created by Philip Rogers on 6/2/22.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text("Map")
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
